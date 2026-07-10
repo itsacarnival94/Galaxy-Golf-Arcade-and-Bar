411412e069f500fef4768f7f3b86f12a14b6a2d7
itsacarnival94 <pinkcoder12@gmail.com>
2026-07-10 15:46:23 -0700

    backend

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE user_data;
+USE user_data;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+    email VARCHAR(100),
+    phone VARCHAR(20)
+
+);
